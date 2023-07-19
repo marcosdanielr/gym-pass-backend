b0d1e0fc170b3d6fe37257e3ae7bb706affafafd
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-19 01:39:13 -0300

    FEAT: check-in use case started

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,31 +3,31 @@ import { Prisma } from '@prisma/client';
 import { UsersRepository } from '../users-repository';
 
 export class PrismaUsersRepository implements UsersRepository {
-  async findById(id: string) {
-    const user = await prisma.user.findUnique({
-      where: {
-        id
-      }
-    });
+    async findById(id: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                id
+            }
+        });
 
-    return user;
-  }
+        return user;
+    }
 
-  async findByEmail(email: string) {
-    const user = await prisma.user.findUnique({
-      where: {
-        email
-      }
-    });
+    async findByEmail(email: string) {
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
 
-    return user;
-  }
+        return user;
+    }
 
-  async create(data: Prisma.UserCreateInput) {
-    const user = await prisma.user.create({
-      data
-    });
+    async create(data: Prisma.UserCreateInput) {
+        const user = await prisma.user.create({
+            data
+        });
 
-    return user;
-  }
+        return user;
+    }
 }
