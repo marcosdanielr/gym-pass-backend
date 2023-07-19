c44b89788cc11a741e087f1e655a8963072264bc
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-19 01:16:34 -0300

    CHORE: controllers using factory pattern

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,36 +1,34 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 import { Status } from '../status';
-import { RegisterUseCase } from '@/use-cases/register';
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
+import { makeRegisterUseCase } from '@/use-cases/factories/make-register-use-case';
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
-    const registerBodySchema = z.object({
-        name: z.string(),
-        email: z.string().email(),
-        password: z.string().min(6)
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+  });
+
+  const { name, email, password } = registerBodySchema.parse(request.body);
+
+  try {
+    const registerUseCase = makeRegisterUseCase();
+
+    const { user } = await registerUseCase.execute({
+      name, email, password
     });
 
-    const { name, email, password } = registerBodySchema.parse(request.body);
-
-    const prismaUsersRepository = new PrismaUsersRepository();
-    const registerUseCase = new RegisterUseCase(prismaUsersRepository);
-
-    try {
-        const { user } = await registerUseCase.execute({
-            name, email,password
-        });
-
-        return reply.status(Status.CREATED).send({
-            ...user,
-            password_hash: undefined
-        });
-    } catch (err) {
-        if (err instanceof UserAlreadyExistsError) {
-            return reply.status(Status.CONFLICT).send({message: err.message});
-        }
-
-        throw err;
+    return reply.status(Status.CREATED).send({
+      ...user,
+      password_hash: undefined
+    });
+  } catch (err) {
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(Status.CONFLICT).send({ message: err.message });
     }
+
+    throw err;
+  }
 }
