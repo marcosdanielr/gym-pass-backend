763382967d0f0f3d666a0538d1249a02bc65fd8e
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-19 01:33:09 -0300

    FEAT: get user by id use case with unit tests

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,21 +3,31 @@ import { Prisma } from '@prisma/client';
 import { UsersRepository } from '../users-repository';
 
 export class PrismaUsersRepository implements UsersRepository {
-    async findByEmail(email: string) {
-        const user = await prisma.user.findUnique({
-            where: {
-                email
-            }
-        });
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id
+      }
+    });
 
-        return user;
-    }
+    return user;
+  }
 
-    async create(data: Prisma.UserCreateInput) {
-        const user = await prisma.user.create({
-            data    
-        });
+  async findByEmail(email: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    });
 
-        return user;
-    }
+    return user;
+  }
+
+  async create(data: Prisma.UserCreateInput) {
+    const user = await prisma.user.create({
+      data
+    });
+
+    return user;
+  }
 }
---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,41 @@
+import { it, expect, describe, beforeEach } from 'vitest';
+import { hash } from 'bcryptjs';
+import { GetUserProfileUseCase } from './get-user-profile';
+import { ResourceNotFoundError } from './errors/resource-not-found-error';
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
+
+let usersRepository: InMemoryUsersRepository;
+let sut: GetUserProfileUseCase;
+
+describe('Get User Profile UseCase', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository();
+    sut = new GetUserProfileUseCase(usersRepository);
+  })
+
+  it('should be able to get user profile', async () => {
+
+    const name = 'Test';
+
+    const createdUser = await usersRepository.create({
+      name,
+      email: 'testeeee@email.com',
+      password_hash: await hash('12345678910', 6)
+    });
+
+    await sut.execute({
+      userId: createdUser.id
+    });
+
+
+    expect(createdUser.id).toEqual(expect.any(String));
+    expect(createdUser.name).toEqual(name);
+  });
+
+  it('should not be able to get user profile with wrong id', async () => {
+
+    expect(() => sut.execute({
+      userId: 'non-existing-id'
+    })).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+});
