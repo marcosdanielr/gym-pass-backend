ae0b54b8ec42840b213d422b176c7b872f5e9002
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-23 19:48:31 -0300

    FEAT: prisma-test-environment package created

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -0,0 +1,15 @@
+import { Environment } from 'vitest';
+
+export default <Environment>{
+    name: 'prisma',
+    async setup() {
+        console.log('Setup');
+
+        return {
+            async teardown() {
+                console.log('Teardown');
+            }
+        };
+    }
+
+};
