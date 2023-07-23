3292108b4b096e4c1a5fa3fe2e9364ca019919ba
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-23 17:51:25 -0300

    FEAT: prisma repository find many nearby gyms

--- src/repositories/prisma/prisma-gyms-repository.ts
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -1,4 +1,4 @@
-import { Prisma } from '@prisma/client';
+import { Gym, Prisma } from '@prisma/client';
 import { FindManyNearbyParams, GymsRepository } from '../gyms-repository';
 import { prisma } from '@/lib/prisma';
 
@@ -33,10 +33,14 @@ export class PrismaGymsRepository implements GymsRepository {
         });
 
         return gyms;
-      
     }
 
-    async findManyNearby(params: FindManyNearbyParams) {
-      
+    async findManyNearby({latitude, longitude}: FindManyNearbyParams) {
+        const gyms = await prisma.$queryRaw<Gym[]>`
+           SELECT * FROM gyms 
+           WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+        `;
+
+        return gyms;
     }
 }
