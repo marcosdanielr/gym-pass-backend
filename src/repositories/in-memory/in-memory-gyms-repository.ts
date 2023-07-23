181e0f96fc2fe39c671d95263a7b291a89a8bb89
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-22 23:26:29 -0300

    FEAT: create-gym use-case

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,5 +1,6 @@
-import { Gym } from '@prisma/client';
+import { Gym, Prisma } from '@prisma/client';
 import { GymsRepository } from '../gyms-repository';
+import { randomUUID } from 'crypto';
 
 export class InMemoryGymsRepository implements GymsRepository {
     public gyms: Gym[] = [];
@@ -10,6 +11,23 @@ export class InMemoryGymsRepository implements GymsRepository {
         if (!gym) {
             return null;
         }
+
+        return gym;
+    }
+
+
+    async create(data: Prisma.GymCreateInput) {
+        const gym = {
+            id: randomUUID(),
+            title: data.title,
+            description: data.description ?? null,
+            phone: data.phone ?? null,
+            latitude: new Prisma.Decimal(data.latitude.toString()),
+            longitude: new Prisma.Decimal(data.longitude.toString()),
+            created_at: new Date()
+        };
+
+        this.gyms.push(gym);
 
         return gym;
     }
