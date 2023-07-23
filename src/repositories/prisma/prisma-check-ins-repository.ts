b92779489b653fd9c639a9011a85e351f8dfafba
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-23 15:15:31 -0300

    FEAT: prisma find check-in by user on date

--- src/repositories/prisma/prisma-check-ins-repository.ts
+++ src/repositories/prisma/prisma-check-ins-repository.ts
@@ -1,6 +1,7 @@
 import { prisma } from '@/lib/prisma';
 import { CheckInsRepository } from '../check-ins-repository';
 import { CheckIn, Prisma } from '@prisma/client';
+import dayjs from 'dayjs';
 
 export class PrismaCheckInsRepository implements CheckInsRepository {
     async create(data: Prisma.CheckInUncheckedCreateInput) {
@@ -54,8 +55,19 @@ export class PrismaCheckInsRepository implements CheckInsRepository {
     }
 
     async findByUserIdOnDate(userId: string, date: Date) {
-        return null;
+        const startOfTheDay = dayjs(date).startOf('date');
+        const endOfTheDay = dayjs(date).endOf('date');
+
+        const checkIn = await prisma.checkIn.findFirst({
+            where: {
+                user_id: userId,
+                created_at: {
+                    gte: startOfTheDay.toDate(),
+                    lte: endOfTheDay.toDate()
+                }
+            }
+        });
+
+        return checkIn;
     }
-
-
 }
