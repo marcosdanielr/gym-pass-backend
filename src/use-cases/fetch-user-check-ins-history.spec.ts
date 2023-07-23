b50693c2c475fb9504f11af188aa7d7c98e80c64
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-23 01:03:38 -0300

    TEST: check-ins history pagination

--- src/use-cases/fetch-user-check-ins-history.spec.ts
+++ src/use-cases/fetch-user-check-ins-history.spec.ts
@@ -23,13 +23,35 @@ describe('Fetch User Check-in History Use Case', () => {
         });
 
         const { checkIns } = await sut.execute({
-            userId: 'user-01'
+            userId: 'user-01',
+            page: 1
         });
 
         expect(checkIns).toHaveLength(2);
         expect(checkIns).toEqual([
             expect.objectContaining({gym_id: 'gym-01'}),
             expect.objectContaining({gym_id: 'gym-02'}),
+        ]);
+    });
+
+
+    it('should be able to fetch paginated check-in history', async () => {
+        for (let i = 1; i <= 22; i++) {
+            await checkInsRepository.create({
+                gym_id: `gym-${i}`,
+                user_id: 'user-01'
+            });
+        }
+
+        const { checkIns } = await sut.execute({
+            userId: 'user-01',
+            page: 2
+        });
+
+        expect(checkIns).toHaveLength(2);
+        expect(checkIns).toEqual([
+            expect.objectContaining({gym_id: 'gym-21'}),
+            expect.objectContaining({gym_id: 'gym-22'}),
         ]);
     });
 });
