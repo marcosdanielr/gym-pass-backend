76dd3b975467139b32fb223d2619070632854b53
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-23 21:46:32 -0300

    FEAT: search gyms controller

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,22 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { z } from 'zod';
+import { Status } from '../../status';
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case';
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+    const searchGymsQuerySchema = z.object({
+        query: z.string(),
+        page: z.coerce.number().min(1).default(1)
+    });
+
+    const { query, page } = searchGymsQuerySchema.parse(request.body);
+
+    const searchGymsUseCase = makeSearchGymsUseCase();
+
+    const gyms = await searchGymsUseCase.execute({
+        query,
+        page, 
+    });
+
+    return reply.status(Status.CREATED).send({gyms});
+}
