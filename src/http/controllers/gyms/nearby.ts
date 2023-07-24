31068c7b4d58e87b42f1721fd13f598631d610d2
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-23 21:54:27 -0300

    FIX: fetch nearby gyms controller function name

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -1,10 +1,9 @@
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { z } from 'zod';
 import { Status } from '../../status';
-import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case';
 import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms';
 
-export async function search(request: FastifyRequest, reply: FastifyReply) {
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
     const nearbyGymsQuerySchema = z.object({
         latitude: z.number().refine(value => {
             return Math.abs(value) <= 90;
