51f81e1c4ea18d399194121e115314036706bcdc
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-26 00:04:36 -0300

    FEAT: create and auth test util

---
+++ src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,24 @@
+import { FastifyInstance } from 'fastify';
+import request from 'supertest';
+
+export async function createAndAuthenticateUser(app: FastifyInstance) {
+    const userCredentials = {
+        email: 'test@test.com',
+        password: '123456'
+    };
+
+    await request(app.server)
+        .post('/users')
+        .send({
+            name: 'Testing',
+            ...userCredentials
+        });
+    
+    const authResponse = await request(app.server)
+        .post('/sessions')
+        .send(userCredentials);
+
+    const { token } = authResponse.body;
+
+    return token;
+}
