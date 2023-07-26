7fd76c3f3d269611083f1155d78583a4117ba43b
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-26 00:20:43 -0300

    TEST: create gym (E2E)

---
+++ src/http/controllers/gyms/create.spec.ts
@@ -0,0 +1,36 @@
+import request from 'supertest';
+import { app } from '@/app';
+import { it, describe, expect, beforeAll, afterAll } from 'vitest';
+import { Status } from '../../status';
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user';
+
+describe('Create Gym (e2e)', () => {
+    beforeAll(async() => {
+        await app.ready();
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    it('should be able to create a gym', async () => {
+
+        const { token } = await createAndAuthenticateUser(app);
+
+        const response = await request(app.server)
+            .post('/gyms')
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                title: 'Testing',
+                description: '^^',
+                phone: '9898989899',
+                latitude: 35.8113216,
+                longitude: -109.6255108
+            });
+
+        expect(response.statusCode).toEqual(Status.CREATED);
+        expect(response.body.gym).toEqual(expect.objectContaining({
+            id: expect.any(String)
+        }));
+    });
+});
