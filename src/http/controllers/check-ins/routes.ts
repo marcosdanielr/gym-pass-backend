04b36c7535806e7fcf292c9e098765875b42de3c
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-25 23:57:48 -0300

    FEAT: validate check-in controller

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from 'fastify';
 
 import { verifyJWT } from '@/http/middlewares/verify-jwt';
 import { create } from 'domain';
+import { validate } from './validate';
 
 
 
@@ -9,4 +10,5 @@ export async function checkInsRoutes(app: FastifyInstance) {
     app.addHook('onRequest', verifyJWT);
 
     app.post('gyms/:gymId/check-ins', create);
+    app.post('check-ins/:checkInId/validate', validate);
 }
