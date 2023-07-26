05db2d43500a813d25a10e6958559247a3a75dbb
Marcos Daniel <marcosdaniel99@protonmail.com>
2023-07-25 23:59:21 -0300

    CHORE: history and metrics route in check-ins

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -3,12 +3,15 @@ import { FastifyInstance } from 'fastify';
 import { verifyJWT } from '@/http/middlewares/verify-jwt';
 import { create } from 'domain';
 import { validate } from './validate';
-
-
+import { history } from './history';
+import { metrics } from './metrics';
 
 export async function checkInsRoutes(app: FastifyInstance) {
     app.addHook('onRequest', verifyJWT);
-
+  
+    app.get('check-ins/history', history);
+    app.get('check-ins/metrics', metrics);
+  
     app.post('gyms/:gymId/check-ins', create);
     app.post('check-ins/:checkInId/validate', validate);
 }
